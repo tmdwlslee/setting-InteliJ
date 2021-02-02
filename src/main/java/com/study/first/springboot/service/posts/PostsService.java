fadccb8f5a209b7eefb4491d3f2a34ef8431b5ec
tmdwlslee <tmdwlsclrn@naver.com>
2021-02-02 22:35:03 +0900

    머스테치 기본 사용법, 스프링 부트 화면처리 방식

--- src/main/java/com/study/first/springboot/service/posts/PostsService.java
+++ src/main/java/com/study/first/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.study.first.springboot.service.posts;
 
 import com.study.first.springboot.domain.posts.PostRepository;
 import com.study.first.springboot.domain.posts.Posts;
+import com.study.first.springboot.web.dto.PostsListResponseDto;
 import com.study.first.springboot.web.dto.PostsResponseDto;
 import com.study.first.springboot.web.dto.PostsSaveRequestDto;
 import com.study.first.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +38,16 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
+        postRepository.delete(posts);
     }
 }
